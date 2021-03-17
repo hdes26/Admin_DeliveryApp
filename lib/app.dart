9929b704c9f749663cfbb108eac5595a4e49fd04
Martin Jimenez <majiar16@gmail.com>
2021-03-16 22:46:57 -0500

    implementacion rutas

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/admin_page.dart';
+import 'package:flutter_application_1/pages/pedidosPendientes_page.dart';
 
-class MyApp extends StatelessWidget{
-@override
+class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-  
-      home: Center(child: AdminPage(),
-      ),
-      
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => AdminPage(),
+        'pedidosPendientes': (BuildContext context) => PedidosPendientes(),
+      },
     );
   }
 }
---
+++ lib/pages/pedidosPendientes_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PedidosPendientes extends StatefulWidget {
+  PedidosPendientes({Key key}) : super(key: key);
+
+  @override
+  _PedidosPendientesState createState() => _PedidosPendientesState();
+}
+
+class _PedidosPendientesState extends State<PedidosPendientes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("xd"),
+      ),
+    );
+  }
+}
