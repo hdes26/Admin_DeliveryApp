62a39fd55eef2dbbd456c79cf935446490357437
Martin Jimenez <majiar16@gmail.com>
2021-04-17 16:51:20 -0500

    crud categorys

--- lib/pages/categorias_page.dart
+++ lib/pages/categorias_page.dart
@@ -18,10 +18,13 @@ class Category extends StatefulWidget {
 
 class CategoryState extends State<Category> {
   final formKey = GlobalKey<FormState>();
+  @override
+  void setState(fn) {
+    super.setState(fn);
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
         appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.grey),
@@ -30,90 +33,90 @@ class CategoryState extends State<Category> {
           centerTitle: true,
           title: Text("Categorias", style: TextStyle(color: Colors.grey)),
         ),
-
+        bottomNavigationBar: _button(context),
         body: Container(
           color: Colors.white,
           height: double.infinity,
           child: _builderCategorias(context),
-    
-        )
-        );
-          }
-}
+        ));
+  }
 
-Widget _category(Categoria data, BuildContext context) {
-  return Column(
-    children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-         Text(data.nombre, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-         Row(children: [
-
-         
-          InkWell(
-            child: Container(
-              width: 50,
-              height: 50,
-              color: Colors.yellow,
-              child: Icon(Icons.edit, size: 40),
-            ),
-            onTap: () => {Navigator.pushNamed(context, 'editCategory',arguments: {
-              "id":data.id,
-              "nombre":data.nombre
-            })},
-          ),
-          SizedBox(
-            width: 30,
-          ),
-          InkWell(
-            child: Container(
-              width: 50,
-              height: 50,
-              color: Colors.red,
-              child: Icon(Icons.delete_forever, size: 40),
-            ),
-            onTap: () => Navigator.pushNamed(context, 'eliminarcategoria', arguments: {
-                    "id": data.id,
-                  })
-                  
-      )],
-      )]
-      ),
-      
-      
+  Widget _category(Categoria data, BuildContext context) {
+    final infoProvider = Provider.of<InfoProvider>(context);
+    return InkWell(
+      child: Dismissible(
+        direction: DismissDirection.endToStart,
+        key: Key(data.id),
+        background: Container(
+          color: Color(0x9DEB1515),
+        ),
+        onDismissed: (direccion) {
+          CategoriaProvider().delete(data.id, infoProvider.token);
+        },
+        child: Column(
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Text(data.nombre,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ]),
             Divider(
-        color: Colors.black,
-        height: 27,
-        thickness: 1.1,
-      ),],
-      );
+              color: Colors.black,
+              height: 27,
+              thickness: 0.2,
+            ),
+          ],
+        ),
+      ),
+      onTap: () async {
+        await Navigator.pushNamed(context, 'editCategory',
+            arguments: {"id": data.id, "nombre": data.nombre});
+        setState(() => {});
+      },
+    );
+  }
 
-  
-}
+  Widget _builderCategorias(BuildContext context) {
+    final infoProvider = Provider.of<InfoProvider>(context);
+    return FutureBuilder(
+      future: CategoriaProvider().getAll(infoProvider.token),
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        if (snapshot.hasData) {
+          // return Text("hola");
+          return ListView.builder(
+            physics: ScrollPhysics(parent: ScrollPhysics()),
+            shrinkWrap: true,
+            itemCount: snapshot.data.length,
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            itemBuilder: (BuildContext context, int index) {
+              return _category(snapshot.data[index], context);
+            },
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
 
-
-Widget _builderCategorias(BuildContext context) {
-  final infoProvider = Provider.of<InfoProvider>(context);
-  return FutureBuilder(
-    future: CategoriaProvider().getAll(infoProvider.token),
-    builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-      if (snapshot.hasData) {
-        // return Text("hola");
-        return ListView.builder(
-          physics: ScrollPhysics(parent: ScrollPhysics()),
-          shrinkWrap: true,
-          itemCount: snapshot.data.length,
-          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-          itemBuilder: (BuildContext context, int index) {
-            return _category(snapshot.data[index], context);
-          },
-        );
-      } else {
-        return Center(
-          child: CircularProgressIndicator(),
-        );
-      }
-    },
-  );
+  Widget _button(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(primary: Color(0xF2EB1515)),
+        onPressed: () async {
+          await Navigator.pushNamed(context, 'registroCategoria');
+          setState(() {});
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Text(
+            'Registrar Categoria',
+            style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
 }
--- lib/providers/categoriasProvider.dart
+++ lib/providers/categoriasProvider.dart
@@ -21,4 +21,48 @@ class CategoriaProvider {
     final categoria = new Categoria.fromJsonMap(decodeData['category']);
     return categoria;
   }
+
+  Future<String> create(String token, String nombre) async {
+    final url = Uri.https(_url, '/api/category/create');
+
+    var response = await http.post(url, headers: {
+      'x-access-token': token, // set content-length
+    }, body: {
+      "nombre": nombre
+    });
+    final decodeData = json.decode(response.body);
+    print(decodeData['message']);
+    // final response = await http.post("POST", url, headers: {
+    //   'x-access-token': token,
+    // }, body: {});
+    // print(response.body);
+    // final decodeData = json.decode(response.body);
+    // final plato = new Plato.fromJsonMap(decodeData['plato']);
+    // print(plato);
+    return decodeData['message'];
+  }
+
+  delete(String id, String token) async {
+    String endpoint = '/api/category/remove/' + id;
+    final url = Uri.https(_url, endpoint);
+
+    final response = await http.delete(url, headers: {'x-access-token': token});
+
+    final decodeData = json.decode(response.body);
+
+    return decodeData['message'].toString();
+  }
+
+  update(String id, String nombre, String token) async {
+    final url = Uri.https(_url, '/api/category/update/' + id);
+    final response = await http.put(url, headers: {
+      'x-access-token': token
+    }, body: {
+      "nombre": nombre,
+    });
+    print(response.body);
+    final decodeData = json.decode(response.body);
+
+    return decodeData["message"];
+  }
 }
